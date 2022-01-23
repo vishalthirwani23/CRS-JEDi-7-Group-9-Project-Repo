d54eda94745efef45cdafdd199ac684164baffb1
Vishal Thirwani <54448159+vishalthirwani23@users.noreply.github.com>
2022-01-24 00:12:47 +0530

    Update and rename UserInterface.java to ReportCardInterface.java

---
+++ CRS-Jedi-Flipkart-POS/src/com/crs/flipkart/business/ReportCardInterface.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.flipkart.business;
+
+import java.util.HashMap;
+
+import com.flipkart.bean.RegisteredCourses;
+import com.flipkart.bean.ReportCard;
+
+/**
+ * @author Asus
+ *
+ */
+public interface ReportCardInterface {
+	
+	/**
+	 * Method to get student's SGPA
+	 * @param studentId
+	 * @param semesterId
+	 * @return sgpa for that semester and student
+	 */
+	public double getSGPA(int studentId, int semesterId) ;
+	
+	
+	
+//	/**
+//	 * Method to get student's grades for a course
+//	 * @param studentId
+//	 * @param semesterId
+//	 * @param courseId
+//	 * @return student's grades for that course 
+//	 */
+	public HashMap<String, Integer> viewCourseGrade(int studentId, int semesterId, int courseId);
+	
+}
