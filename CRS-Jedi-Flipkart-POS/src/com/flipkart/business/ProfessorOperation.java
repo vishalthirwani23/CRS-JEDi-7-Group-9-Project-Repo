b2ac48a1b909dbeab53b9cd936c31e9ebe66ca87
Kotikalapudi Venkat Karthik <venkat.karthik@BLRETV-C02FFN25.local>
2022-01-25 18:34:24 +0530

    buisness additions

---
+++ CRS-Jedi-Flipkart-POS/src/com/flipkart/business/ProfessorOperation.java
@@ -0,0 +1,64 @@
+package com.flipkart.business;
+
+/**
+ * @author venkat.karthik
+ *
+ */
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.flipkart.bean.Course;
+
+
+public class ProfessorOperation implements ProfessorInterface {
+
+	private static volatile ProfessorOperation instance = null;
+	ProfessorDaoInterface professorDAOInterface = ProfessorDaoOperation.getInstance();
+
+	private ProfessorOperation() {
+
+	}
+
+	public static ProfessorOperation getInstance() {
+		if (instance == null) {
+			synchronized (ProfessorOperation.class) {
+				instance = new ProfessorOperation();
+			}
+		}
+		return instance;
+	}
+
+	public boolean addGrade(int studentId, String courseCode, String grade) throws Exception {
+		try {
+			professorDAOInterface.addGrade(studentId, courseCode, grade);
+		} catch (Exception ex) {
+			throw new Exception();
+		}
+		return true;
+	}
+
+	public List<EnrolledStudent> viewEnrolledStudents(String profId) throws SQLException {
+		List<EnrolledStudent> enrolledStudents = new ArrayList<EnrolledStudent>();
+		try {
+			enrolledStudents = professorDAOInterface.getEnrolledStudents(profId);
+		} catch (Exception ex) {
+			throw ex;
+		}
+		return enrolledStudents;
+	}
+
+	public List<Course> getCourses(String profId) {
+		List<Course> coursesOffered = new ArrayList<Course>();
+		try {
+			coursesOffered = professorDAOInterface.getCoursesByProfessor(profId);
+		} catch (Exception ex) {
+			throw ex;
+		}
+		return coursesOffered;
+	}
+
+	public String getProfessorById(String profId) {
+		return professorDAOInterface.getProfessorById(profId);
+	}
+}
--- CRS-Jedi-Flipkart-POS/src/com/flipkart/business/ReportCardInterface.java
+++ CRS-Jedi-Flipkart-POS/src/com/flipkart/business/ReportCardInterface.java
@@ -7,28 +7,15 @@ import java.util.HashMap;
 
 
 /**
- * @author Asus
+ * @author venkat.karthik
  *
  */
 public interface ReportCardInterface {
 	
-	/**
-	 * Method to get student's SGPA
-	 * @param studentId
-	 * @param semesterId
-	 * @return sgpa for that semester and student
-	 */
+
 	public double getSGPA(int studentId, int semesterId) ;
 	
-	
-	
-//	/**
-//	 * Method to get student's grades for a course
-//	 * @param studentId
-//	 * @param semesterId
-//	 * @param courseId
-//	 * @return student's grades for that course 
-//	 */
+
 	public HashMap<String, Integer> viewCourseGrade(int studentId, int semesterId, int courseId);
 	
 }
