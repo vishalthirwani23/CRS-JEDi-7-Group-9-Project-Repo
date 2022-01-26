2d2f29636aba35de6b3d7d5e49813ca7a5d38b48
Tushar Jaiswal <tusharjaiswal1717@gmail.com>
2022-01-26 16:26:46 +0530

    dao files

---
+++ CRS-Jedi-Flipkart-POS/src/com/flipkart/dao/StudentDaoInterface.java
@@ -0,0 +1,23 @@
+
+package com.flipkart.dao;
+import java.sql.SQLException;
+
+import com.flipkart.bean.Student;
+import com.flipkart.exception.StudentNotRegisteredException;
+
+//Interface for Student Operations
+
+public interface StudentDaoInterface {
+
+	//Method to add student to database
+	
+	public int addStudent(Student student) throws Exception;
+
+	// Method to retrieve Student Id from User Id
+	
+	public int getStudentId(String userId);
+
+	// Method to check if Student is approved
+	
+	public boolean isApproved(int studentId);
+}
