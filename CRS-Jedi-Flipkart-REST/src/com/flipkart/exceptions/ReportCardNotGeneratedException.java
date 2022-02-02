f675dc6dd4f553b0452617c6d8012fc3db8b2191
unknown <mehakgoel@2723@gmail.com>
2022-02-02 09:11:54 +0530

    Duplicates rm

--- CRS-Jedi-Flipkart-REST/src/com/flipkart/exceptions/ReportCardNotGeneratedException.java
+++
@@ -1,8 +0,0 @@
-package com.flipkart.exceptions;
-
-public class ReportCardNotGeneratedException extends Exception {
-	public String getMessage() {
-		// TODO Auto-generated method stub
-		return "Report Card Not Generated !";
-	}
-}
